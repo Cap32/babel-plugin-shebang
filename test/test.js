c602dece4886c7100d2d7fbd1d9f648bd4eb3b6d
3went2 <webb.png@gmail.com>
2018-11-20 18:45:17 +0800

    add babel v7 testing

--- test/test.js
+++ test/test.js
@@ -1,37 +1,42 @@
+require("string.prototype.startswith");
+const shebang = require("../");
 
-require('string.prototype.startswith');
-const shebang = require('../');
-const transform = require('babel-core').transform;
+const run = (name, transform) => {
+	describe(name, () => {
+		test("default", () => {
+			const src = "#!/usr/bin/env ./node_modules/.bin/babel-node";
+			const dest = transform(src, {
+				plugins: [shebang]
+			});
+			expect(dest.code.startsWith("#!/usr/bin/env node")).toBe(true);
+		});
 
-test('default', () => {
-	const src = '#!/usr/bin/env ./node_modules/.bin/babel-node';
-	const dest = transform(src, {
-		plugins: [shebang],
+		test("`replacement` option", () => {
+			const src = "#!/usr/bin/env node";
+			const replacement = "#!awesome";
+			const dest = transform(src, {
+				plugins: [[shebang, { replacement }]]
+			});
+			expect(dest.code.startsWith(replacement)).toBe(true);
+		});
+
+		test("without shebang", () => {
+			const src = "";
+			const dest = transform(src, {
+				plugins: [shebang]
+			});
+			expect(dest.code.startsWith("#!/usr/bin/env node")).toBe(false);
+		});
+
+		test("`force` option", () => {
+			const src = "";
+			const dest = transform(src, {
+				plugins: [[shebang, { force: true }]]
+			});
+			expect(dest.code.startsWith("#!/usr/bin/env node")).toBe(true);
+		});
 	});
-	expect(dest.code.startsWith('#!/usr/bin/env node')).toBe(true);
-});
+};
 
-test('`replacement` option', () => {
-	const src = '#!/usr/bin/env node';
-	const replacement = 'awesome';
-	const dest = transform(src, {
-		plugins: [[shebang, { replacement }]],
-	});
-	expect(dest.code.startsWith(replacement)).toBe(true);
-});
-
-test('without shebang', () => {
-	const src = '';
-	const dest = transform(src, {
-		plugins: [shebang],
-	});
-	expect(dest.code.startsWith('#!/usr/bin/env node')).toBe(false);
-});
-
-test('`force` option', () => {
-	const src = '';
-	const dest = transform(src, {
-		plugins: [[shebang, { force: true }]],
-	});
-	expect(dest.code.startsWith('#!/usr/bin/env node')).toBe(true);
-});
+run("babel v6", require("babel-core").transform);
+run("babel v7", require("@babel/core").transform);
